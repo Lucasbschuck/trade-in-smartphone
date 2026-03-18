c162552f7cc4a33896edb8c1a51cf8099503d08a
Lucas Braga Schuck <lucasbschuck1708@gmail.com>
2026-03-18 10:07:00 -0300

    initial commit

---
+++ trade-in-smartphone-service/src/main/java/com/github/lucasbschuck/TradeInSmartphoneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasbschuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeInSmartphoneServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeInSmartphoneServiceApplication.class, args);
+    }
+
+}
