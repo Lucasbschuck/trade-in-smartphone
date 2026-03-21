c15280d14c0da6e003dacd4c4de8d6884a4a3a97
Lucas Braga Schuck <lucasbschuck1708@gmail.com>
2026-03-21 13:11:06 -0300

    feat: Add create Smartphone feature and Connection with DataBase

---
+++ trade-in-smartphone-service/src/main/java/com/github/lucasbschuck/TradeInSmartphoneServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.lucasbschuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class TradeInSmartphoneServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeInSmartphoneServiceApplication.class, args);
+    }
+
+}
